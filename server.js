bdf51001c5567c7c8ac5d5e81c4ac6d9829c0cae
root <root@ip-172-31-5-193.ap-south-1.compute.internal>
2024-07-07 05:57:01 +0000

    change1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('batch825 jenkins demo! . Welcome To DevOps session. final approch is good welcome');});
+  res.send('welecom welcome wleocmee');});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
